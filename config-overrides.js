f2009edee420f1fe4e6c1e495f9bb07c50bf1384
Aditya <adiag1200@gmail.com>
2021-05-23 16:30:07 +0530

    remove preact; add webpack plugins

--- config-overrides.js
+++ config-overrides.js
@@ -1,30 +1,11 @@
-const { override, addWebpackAlias } = require("customize-cra");
-const RemoveDuplicate = require("duplicate-package-checker-webpack-plugin");
+const { override } = require("customize-cra");
 const UglifyJS = require("uglifyjs-webpack-plugin");
 const webpack = require("webpack");
-// const CompressPlugin = require("compression");
 
 const addPlugins = (config) => {
-  config.plugins.push(new RemoveDuplicate());
   config.plugins.push(new UglifyJS());
-  //   config.plugins.push(
-  //     new CompressPlugin({
-  //       asset: "[path].gz[query]",
-  //       algorithm: "gzip",
-  //       //   test: /\.js$|\.css$|\.html$/,
-  //       threshold: 10240,
-  //       minRatio: 0.8,
-  //     })
-  //   );
   config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
-  //   config.plugins.push(new webpack.optimize.MinChunkSizePlugin());
   return config;
 };
 
-module.exports = override(
-  addWebpackAlias({
-    ["react"]: "preact/compat",
-    ["react-dom"]: "preact/compat",
-  }),
-  addPlugins
-);
+module.exports = override(addPlugins);
